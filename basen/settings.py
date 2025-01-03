55c5a82cbee1cefefdfddfeccb4c2b681d266d2d
Danylo <danylomoskovchuk@gmail.com>
2025-01-03 15:15:37 +0100

    pool page

--- basen/settings.py
+++ basen/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+
+    'compressor',
     'basen_app'
 ]
 
@@ -117,6 +119,14 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
     "static/",
 ]
+
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+COMPRESS_ENABLED = True
+COMPRESS_PRECOMPILERS = (
+    ('text/x-scss', 'django_libsass.SassCompiler'),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
