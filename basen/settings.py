1318594c1b3e4572d27887e0e10b16bcd3fd41a6
Roman Beznosenko <245125@edu.p.lodz.pl>
2025-01-16 16:55:01 +0100

    updated css in contact page

--- basen/settings.py
+++ basen/settings.py
@@ -35,7 +35,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'compressor',
     'basen_app'
 ]
 
@@ -122,9 +121,3 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-COMPRESS_ENABLED = True
-COMPRESS_PRECOMPILERS = (
-    ('text/x-scss', 'django_libsass.SassCompiler'),
-)
