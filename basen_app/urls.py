03edbd11938f2775b629931f0f5dd701eae905af
Roman Beznosenko <245125@edu.p.lodz.pl>
2025-03-11 20:11:30 +0100

    Created Example Page

--- basen_app/urls.py
+++ basen_app/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('contact/', views.contact, name='contact'),
     path('about-us/', views.aboutUs, name='about-us'),
     path('gallery/', views.gallery, name='gallery'),
-    path('types-of-pools/', views.typesOfPools, name='types-of-pools')
+    path('types-of-pools/', views.typesOfPools, name='types-of-pools'),
+    path('example/', views.example, name='example')
 ]
