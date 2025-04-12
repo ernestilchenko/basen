34df809cfc59ef2a85252234630e1124b5125f9b
ernestilchenko <7777erik777@gmail.com>
2025-04-12 18:42:47 +0200

    fix part 2

--- basen/settings.py
+++ basen/settings.py
@@ -135,8 +135,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'messengerproject64@gmail.com'
-EMAIL_HOST_PASSWORD = 'xpmcnievguhlcues'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 EMAIL_USE_SSL = False
 LOCALE_PATHS = [
     os.path.join(BASE_DIR, 'locale'),
